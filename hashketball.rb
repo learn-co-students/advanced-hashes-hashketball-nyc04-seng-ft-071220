f98475172dcc6b020e7a203fba94ee36d2f25f6b
wilsonng21 <wilsongch92@gmail.com>
2020-07-28 16:54:25 -0400

    all tests passing

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -127,3 +129,262 @@ def game_hash
 end
 
 # Write code here
+
+def num_points_scored(name_input)
+  player_point = 0
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if player[:player_name] == name_input
+      player_point = player[:points]
+      # binding.pry
+    end
+  end
+
+  away_players.each do |player|
+    if player[:player_name] == name_input
+      player_point = player[:points]
+      # binding.pry
+    end
+  end
+
+  player_point
+end
+
+def shoe_size(name_input)
+  player_shoe_size = 0
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if player[:player_name] == name_input
+      player_shoe_size = player[:shoe]
+      # binding.pry
+    end
+  end
+
+  away_players.each do |player|
+    if player[:player_name] == name_input
+      player_shoe_size = player[:shoe]
+      # binding.pry
+    end
+  end
+
+  player_shoe_size
+end
+
+def team_colors(team_name)
+  home_team_name = game_hash[:home][:team_name]
+  away_team_name = game_hash[:away][:team_name]
+  home_team_colors = game_hash[:home][:colors]
+  away_team_colors = game_hash[:away][:colors]
+
+  team_name == home_team_name ? home_team_colors : away_team_colors
+end
+
+def team_names
+  home_team_name = game_hash[:home][:team_name]
+  away_team_name = game_hash[:away][:team_name]
+  [].push(home_team_name, away_team_name)
+end
+
+def player_numbers(team_name)
+  jersey_numbers = []
+
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_team_name = game_hash[:home][:team_name]
+  away_team_name = game_hash[:away][:team_name]
+
+  if team_name == home_team_name
+    home_players.each do |player|
+      jersey_numbers << player[:number]
+      # binding.pry
+    end
+  end
+
+  if team_name == away_team_name
+    away_players.each do |player|
+      jersey_numbers << player[:number]
+      # binding.pry
+    end
+  end
+  jersey_numbers
+end
+
+def player_stats(player_name)
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if player[:player_name] == player_name
+      return player
+    end
+  end
+
+  away_players.each do |player|
+    if player[:player_name] == player_name
+      return player
+    end
+  end
+end
+
+def big_shoe_rebounds
+  biggest_shoe_size = 0
+  most_rebounds = 0
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if biggest_shoe_size < player[:shoe]
+      biggest_shoe_size = player[:shoe]
+      most_rebounds = player[:rebounds]
+    end
+  end
+  most_rebounds
+  
+  away_players.each do |player|
+    if biggest_shoe_size < player[:shoe]
+      biggest_shoe_size = player[:shoe]
+      most_rebounds = player[:rebounds]
+    end
+  end
+  most_rebounds
+end
+
+def most_points_scored
+  most_points = 0
+  player_name = ""
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if most_points < player[:points]
+      most_points = player[:points]
+      player_name = player[:player_name]
+    end
+  end
+  most_points
+  player_name
+
+  
+  away_players.each do |player|
+    if most_points < player[:points]
+      most_points = player[:points]
+      player_name = player[:player_name]
+    end
+  end
+  most_points
+  player_name
+
+  "#{player_name} scored the most points. #{most_points} points total."
+end
+
+p most_points_scored
+
+def winning_team
+  home_points = 0
+  away_points = 0
+  
+  home_team_name = game_hash[:home][:team_name]
+  away_team_name = game_hash[:away][:team_name]
+
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+      home_points += player[:points]
+  end
+
+  away_players.each do |player|
+      away_points += player[:points]
+  end
+
+  # home_points
+  # away_points
+
+  home_points > away_points ? home_team_name : away_team_name
+  # binding.pry
+end
+
+p winning_team + " has the most points."
+
+def player_with_longest_name
+  longest_name = ""
+  name_size = 0
+  
+  home_team_name = game_hash[:home][:team_name]
+  away_team_name = game_hash[:away][:team_name]
+
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if name_size < player[:player_name].size
+      name_size = player[:player_name].size
+      longest_name = player[:player_name]
+    end
+  end
+
+  away_players.each do |player|
+    if name_size < player[:player_name].size
+      name_size = player[:player_name].size
+      longest_name = player[:player_name]
+    end
+  end
+
+  "#{longest_name} has the longest name. It's #{name_size} characters long."
+
+  # binding.pry
+end
+
+p player_with_longest_name
+
+def long_name_steals_a_ton
+  longest_name = ""
+  name_size = 0
+  most_steals = 0
+  player_with_most_steals = ""
+  
+  home_team_name = game_hash[:home][:team_name]
+  away_team_name = game_hash[:away][:team_name]
+
+  home_players = game_hash[:home][:players]
+  away_players = game_hash[:away][:players]
+
+  home_players.each do |player|
+    if name_size < player[:player_name].size
+      name_size = player[:player_name].size
+      longest_name = player[:player_name] 
+    end
+  end
+
+  away_players.each do |player|
+    if name_size < player[:player_name].size
+      name_size = player[:player_name].size
+      longest_name = player[:player_name]
+    end
+  end
+
+  home_players.each do |player|
+    if most_steals < player[:steals]
+      most_steals = player[:steals]
+      player_with_most_steals = player[:player_name]
+    end
+  end
+
+  away_players.each do |player|
+    if most_steals < player[:steals]
+      most_steals = player[:steals]
+      player_with_most_steals = player[:player_name]
+    end
+  end
+
+  player_with_most_steals == longest_name
+
+end
+
+p long_name_steals_a_ton
+
